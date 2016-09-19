d9506719e7285c52d0a9d1f8fb106939fd2059dc
Kerry Liu <kerryliu@gmail.com>
2016-09-19 19:12:59 +0000

    Update migration script

--- migrations/wp-db/0001.sh
+++ migrations/wp-db/0001.sh
@@ -13,8 +13,9 @@ if [ `hostname` == "db1.g2.gm.a8c.com" ]; then
 	/etc/init.d/mysql1-0 start
 	/usr/local/mysql/bin/mysqladmin --defaults-file=/etc/mysql/mysql1-0.cnf -u root password 'god'
 	mysql --defaults-file=/etc/mysql/mysql1-0.cnf -u root -sN  -pgod -e "CREATE DATABASE wp;"
-	mysql --defaults-file=/etc/mysql/mysql1-0.cnf -u root -sN  -pgod -e "CREATE USER 'wp'@'localhost' IDENTIFIED BY 'drupal';"
-	mysql --defaults-file=/etc/mysql/mysql1-0.cnf -u root -sN  -pgod -e "GRANT ALL ON wp.* TO 'wp'@'localhost';"
+	mysql --defaults-file=/etc/mysql/mysql1-0.cnf -u root -sN  -pgod -e "CREATE USER 'wp'@'192.0.95.%' IDENTIFIED BY 'drupal';"
+	mysql --defaults-file=/etc/mysql/mysql1-0.cnf -u root -sN  -pgod -e 'CREATE USER "repl"@"192.0.95.%" IDENTIFIED BY "0c76b357a9f84a12e6281f1ad0685fcd";
+GRANT REPLICATION SLAVE ON *.* TO "repl"@"192.0.95.%";'
 else
 	ln -sf /etc/mysql-initscripts/mysql-5.6 /etc/init.d/mysql1-1
 	ln -sf /usr/local/mysql-latest /usr/local/mysql
@@ -27,7 +28,4 @@ else
 	chown -R mysql.mysql /var/lib/mysql1-1
 	/etc/init.d/mysql1-1 start
 	/usr/local/mysql/bin/mysqladmin --defaults-file=/etc/mysql/mysql1-1.cnf -u root password 'god'
-	mysql --defaults-file=/etc/mysql/mysql1-1.cnf -u root -sN  -pgod -e "CREATE DATABASE wp;"
-	mysql --defaults-file=/etc/mysql/mysql1-1.cnf -u root -sN  -pgod -e "CREATE USER 'wp'@'localhost' IDENTIFIED BY 'drupal';"
-	mysql --defaults-file=/etc/mysql/mysql1-1.cnf -u root -sN  -pgod -e "GRANT ALL ON wp.* TO 'wp'@'localhost';"
 fi
